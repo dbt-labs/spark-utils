2e56fad7bea22c5dfb1d672f230df8a5e9079e88
Jeremy Cohen <jeremy@fishtownanalytics.com>
2020-09-24 14:43:36 -0400

    Add snowplow, dbt-utils submodules + testing

--- macros/dbt_utils/cross_db_utils/dateadd.sql
+++ macros/dbt_utils/cross_db_utils/dateadd.sql
@@ -3,21 +3,31 @@
     {% if datepart == 'day' %}
 
         date_add(date({{from_date_or_timestamp}}), {{interval}})
+        
+    {% elif datepart == 'week' %}
+
+        date_add(date({{from_date_or_timestamp}}), {{interval}} * 7)
 
     {% elif datepart == 'month' %}
 
         add_months(date({{from_date_or_timestamp}}), {{interval}})
+    
+    {% elif datepart == 'quarter' %}
+    
+        add_months(date({{from_date_or_timestamp}}), {{interval}} * 3)
         
     {% elif datepart == 'year' %}
     
         add_months(date({{from_date_or_timestamp}}), {{interval}} * 12)
 
-    {% elif datepart in ('hour', 'minute', 'second') %}
+    {% elif datepart in ('hour', 'minute', 'second', 'millisecond', 'microsecond') %}
     
         {%- set multiplier -%} 
             {%- if datepart == 'hour' -%} 3600
             {%- elif datepart == 'minute' -%} 60
-            {%- else -%} 1
+            {%- elif datepart == 'second' -%} 1
+            {%- elif datepart == 'millisecond' -%} (1/1000000)
+            {%- elif datepart == 'microsecond' -%} (1/1000000)
             {%- endif -%}
         {%- endset -%}
 
--- macros/dbt_utils/cross_db_utils/datediff.sql
+++ macros/dbt_utils/cross_db_utils/datediff.sql
@@ -3,21 +3,31 @@
     {% if datepart == 'day' %}
 
         datediff(date({{second_date}}), date({{first_date}}))
+        
+    {% elif datepart == 'week' %}
+
+        floor(datediff(date({{second_date}}), date({{first_date}}))/7)
 
     {% elif datepart == 'month' %}
 
         floor(months_between(date({{second_date}}), date({{first_date}})))
         
+    {% elif datepart == 'quarter' %}
+    
+        floor(months_between(date({{second_date}}), date({{first_date}}))/3)
+        
     {% elif datepart == 'year' %}
     
         floor(months_between(date({{second_date}}), date({{first_date}}))/12)
 
-    {% elif datepart in ('hour', 'minute', 'second') %}
+    {% elif datepart in ('hour', 'minute', 'second', 'millisecond', 'microsecond') %}
     
         {%- set divisor -%} 
             {%- if datepart == 'hour' -%} 3600
             {%- elif datepart == 'minute' -%} 60
-            {%- else -%} 1
+            {%- elif datepart == 'second' -%} 1
+            {%- elif datepart == 'millisecond' -%} (1/1000)
+            {%- elif datepart == 'microsecond' -%} (1/1000000)
             {%- endif -%}
         {%- endset -%}
 
