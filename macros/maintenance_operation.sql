68916bf7dd99249dfe0cc98ea2c7cda8fd02f691
Cor Zuurmond <jczuurmond@protonmail.com>
2021-11-24 11:59:55 +0100

    Fixes for maintenance macro

--- macros/maintenance_operation.sql
+++ macros/maintenance_operation.sql
@@ -1,14 +1,14 @@
 {% macro get_delta_tables() %}
   {% set delta_tables = [] %}
   {% for database in spark__list_schemas('not_used') %}
-    {% for table in spark__list_relations_without_caching('not_used', database[0]) %}
+    {% for table in spark__list_relations_without_caching(database[0]) %}
       {% set db_tablename = database[0] ~ "." ~ table[1] %}
       {% call statement('table_detail', fetch_result=True) -%}
         describe extended {{ db_tablename }}
       {% endcall %}
 
       {% for row in load_result('table_detail').table %}
-        {% if row[0] == 'Provider' and row[1] == 'DELTA' %}
+        {% if row[0]|lower == 'provider' and row[1]|lower == 'delta' %}
           {{ delta_tables.append(db_tablename) }}
         {% endif %}
       {% endfor %}
@@ -20,23 +20,36 @@
 {% macro get_tables() %}
   {% set tables = [] %}
   {% for database in spark__list_schemas('not_used') %}
-    {% for table in spark__list_relations_without_caching('not_used', database[0]) %}
+    {% for table in spark__list_relations_without_caching(database[0]) %}
       {% set db_tablename = database[0] ~ "." ~ table[1] %}
-      {% if spark_get_relation_type(db_tablename) == 'table' %}
-          {{ tables.append(db_tablename) }}
-      {% endif %}
+      {% call statement('table_detail', fetch_result=True) -%}
+        describe extended {{ db_tablename }}
+      {% endcall %}
+
+      {% for row in load_result('table_detail').table %}
+        {% if row[0]|lower == 'type' and row[1]|lower != 'view' %}
+        {{ tables.append(db_tablename) }}
+        {% endif %}
+      {% endfor %}
     {% endfor %}
   {% endfor %}
   {{ return(tables) }}
 {% endmacro %}
 
 {% macro get_statistic_columns(table) %}
-  {% set input_columns = spark__get_columns_in_relation(table) %}
+
+  {% call statement('input_columns', fetch_result=True) %}
+      SHOW COLUMNS IN {{ table }}
+  {% endcall %}
+  {% set input_columns = load_result('input_columns').table %}
+
   {% set output_columns = [] %}
-  
   {% for column in input_columns %}
-    {% if not column.dtype.startswith('struct') %}
-      {{ output_columns.append(column.name) }}
+    {% call statement('column_information', fetch_result=True) %}
+        DESCRIBE TABLE {{ table }} {{ column[0] }}
+    {% endcall %}
+    {% if not load_result('column_information').table[1][1].startswith('struct') and not load_result('column_information').table[1][1].startswith('array')  %}
+      {{ output_columns.append(column[0]) }}
     {% endif %}
   {% endfor %}
   {{ return(output_columns) }}
@@ -59,11 +72,12 @@
 {% macro spark_statistics() %}
 
     {% for table in get_tables() %}
-        {% set columns = get_statistic_columns(table) | join(',') %}
         {% set start=modules.datetime.datetime.now() %}
+        {% set columns = get_statistic_columns(table) | join(',') %}
         {% set message_prefix=loop.index ~ " of " ~ loop.length %}
-        {{ dbt_utils.log_info(message_prefix ~ " Analyzing " ~ table) }}
-        {% do run_query("analyze table " ~ table ~ " compute statistics for columns " ~ columns) %}
+        {% if columns != '' %}
+            {% do run_query("analyze table " ~ table ~ " compute statistics for columns " ~ columns) %}
+        {% endif %}
         {% set end=modules.datetime.datetime.now() %}
         {% set total_seconds = (end - start).total_seconds() | round(2)  %}
         {{ dbt_utils.log_info(message_prefix ~ " Finished " ~ table ~ " in " ~ total_seconds ~ "s") }}
