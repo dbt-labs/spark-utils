37aa6e6eaee66d92c1aa98b27764dd30892ffaba
Cor Zuurmond <jczuurmond@protonmail.com>
2021-12-28 16:01:24 +0100

    Use connection from soda spark

--- tests/test_macros.py
+++ tests/test_macros.py
@@ -6,11 +6,22 @@ from dbt.adapters.factory import get_adapter, register_adapter
 from dbt.clients.jinja import MacroGenerator
 from dbt.config.runtime import RuntimeConfig
 from dbt.context import providers
+from dbt.contracts.connection import ConnectionState
+from dbt.adapters.spark.connections import SparkConnectionManager, PyodbcConnectionWrapper
 from dbt.parser.manifest import ManifestLoader
 from dbt.tracking import User
-
+from sodaspark.scan import Connection
 
 dbt.tracking.active_user = User(os.getcwd())
+
+
+class _SparkConnectionManager(SparkConnectionManager):
+    @classmethod
+    def open(cls, connection):
+        handle = PyodbcConnectionWrapper(Connection())
+        connection.handle = handle
+        connection.state = ConnectionState.OPEN
+        return connection
 
 
 @dataclasses.dataclass(frozen=True)
@@ -25,6 +36,9 @@ config = RuntimeConfig.from_args(args)
 register_adapter(config)
 
 adapter = get_adapter(config)
+
+connection_manager = _SparkConnectionManager(adapter.config)
+adapter.connections = connection_manager
 adapter.acquire_connection()
 
 manifest = ManifestLoader.get_full_manifest(config)
@@ -36,4 +50,3 @@ context = providers.generate_runtime_macro_context(
 )
 
 result = MacroGenerator(macro, context)()
-
