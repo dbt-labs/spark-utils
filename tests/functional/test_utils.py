2a329b78400490a0a61af220a67a1d08cc408519
Jeremy Cohen <jeremy@dbtlabs.com>
2022-05-18 12:47:58 +0200

    Take some educated guesses

---
+++ tests/functional/test_utils.py
@@ -0,0 +1,37 @@
+from dbt.tests.adapter.utils.test_dateadd import BaseDateAdd
+from dbt.tests.adapter.utils.test_datediff import BaseDateDiff
+
+
+class BaseSparkUtilsBackCompat(BaseUtils):
+    # install this repo as a package
+    @pytest.fixture(scope="class")
+    def packages(self):
+        return {"packages": [{"local": os.getcwd()}]}
+    
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {
+            "dispatch": [{
+                "macro_namespace": "dbt_utils",
+                "search_order": ["spark_utils", "dbt_utils"]
+            }]
+        }
+
+    # call the macros from the 'dbt_utils' namespace
+    # instead of the unspecified / global namespace
+    def macro_namespace(self):
+        return "dbt_utils"
+
+
+    # actual test sequence needs to run 'deps' first
+    def test_build_assert_equal(self, project):
+        run_dbt(['deps'])
+        super().test_build_assert_equal(project)
+
+
+class TestDateAdd(BaseDateAdd):
+    pass
+
+
+class TestDateDiff(BaseDateDiff):
+    pass
