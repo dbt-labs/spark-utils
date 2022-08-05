df2934694cf7e9051c5413eadac95135538477cb
Cor Zuurmond <jczuurmond@protonmail.com>
2022-08-05 17:26:14 +0200

    Create and delete table with fixture

--- tests/test_macros.py
+++ tests/test_macros.py
@@ -1,17 +1,26 @@
+import uuid
+
 import pytest
 from dbt.clients.jinja import MacroGenerator
 from pyspark.sql import SparkSession
+
+
+@pytest.fixture
+def simple_table(spark_session: SparkSession) -> str:
+    """Create and delete a simple table used for testing."""
+    table_name = f"default.table_{uuid.uuid4()}".replace("-", "_")
+    spark_session.sql(f"CREATE TABLE {table_name} (id int) USING parquet")
+    yield table_name
+    spark_session.sql(f"DROP TABLE IF EXISTS {table_name}")
 
 
 @pytest.mark.parametrize(
     "macro_generator", ["macro.spark_utils.get_tables"], indirect=True
 )
 def test_create_table(
-    spark_session: SparkSession, macro_generator: MacroGenerator
+    macro_generator: MacroGenerator, simple_table: str
 ) -> None:
     """The `get_tables` macro should return the created table."""
-    expected_table = "default.example"
-    spark_session.sql(f"CREATE TABLE {expected_table} (id int) USING parquet")
     tables = macro_generator()
-    assert expected_table in tables
+    assert simple_table in tables
 
